44913f1e8ceec530bc4887cf8bcf3a29f18fc4bc
Mahmodul Hasan <nicesanto92@gmail.com>
2023-07-16 13:28:21 +0600

    fixing some netlify bugs

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -2,7 +2,7 @@ import Banner from "../HeroSection/Banner";
 import Foods from "../FoodItems/Foods";
 import AboutUs from "./AboutUs";
 import Footer from "../Footer/Footer";
-
+import React from "react";
 const Home = () => {
   return (
     <>
