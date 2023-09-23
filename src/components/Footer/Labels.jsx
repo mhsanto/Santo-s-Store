9512888e2dd783877e00a68c0b6f15096d90d903
Mahmodul Hasan <nicesanto92@gmail.com>
2023-09-23 20:58:25 +0600

    redesigned Santo Store

--- src/components/Footer/Labels.jsx
+++ src/components/Footer/Labels.jsx
@@ -5,22 +5,6 @@ import { styled } from "styled-components";
 const Labels = () => {
   return (
     <LabelContainer>
-      <FlexContainer>
-        <UnorderList>
-          <Link>About Online Food</Link>
-          <Link>Read our blog</Link>
-          <Link>Sign up to deliver</Link>
-          <Link>Add up to deliver</Link>
-          <Link>Add your restaurant</Link>
-        </UnorderList>
-        <UnorderList>
-          <Link>Get help</Link>
-          <Link>Read FAQs</Link>
-          <Link>View all cities</Link>
-          <Link>Add up to deliver</Link>
-          <Link>Restaurant Near me</Link>
-        </UnorderList>
-      </FlexContainer>
       <div className="flex">
         <Link to="/footer/terms">Privacy Policy</Link>
         <Link to="/footer/condition">Terms of Use</Link>
@@ -30,6 +14,7 @@ const Labels = () => {
   );
 };
 const LabelContainer = styled.div`
+  align-self: end;
   display: flex;
   flex-direction: column;
   gap: 2rem;
@@ -53,21 +38,5 @@ const LabelContainer = styled.div`
     }
   }
 `;
-const UnorderList = styled.ul`
-  list-style: none;
-  display: flex;
-  flex-direction: column;
-  gap: 9px;
-`;
-const FlexContainer = styled.div`
-  display: flex;
-  column-gap: 10rem;
-  justify-content: space-between;
-  @media (max-width: 1000px) {
-    gap: 3rem;
-  }
-  @media (max-width: 580px) {
-    gap: 1rem;
-  }
-`;
+
 export default Labels;
